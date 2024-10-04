0f3071442cbb2234abb49ced8a2a39e81cc0acab
karinsukchai <karinsukchai.official@gmail.com>
2024-10-04 20:52:51 +0700

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
